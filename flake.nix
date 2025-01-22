103b9983e28fb8884c9bfb8f311f978c9cbfa886
ElrohirGT <elrohirgt@gmail.com>
2025-01-22 11:53:44 -0600

    feat: Added Flake.nix

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "JUWURA Nix Flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    # System types to support.
+    supportedSystems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
+
+    # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+    # Nixpkgs instantiated for supported system types.
+    nixpkgsFor = forAllSystems (system: import nixpkgs {inherit system;});
+
+    # System packages...
+    frontendPkgs = pkgs: [pkgs.nodejs pkgs.pnpm];
+  in {
+    devShells = forAllSystems (system: let
+      pkgs = nixpkgsFor.${system};
+    in {
+      default = pkgs.mkShell {
+        packages = frontendPkgs pkgs;
+      };
+    });
+  };
+}
